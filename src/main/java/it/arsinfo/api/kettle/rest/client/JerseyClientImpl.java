ef6493abfe8ac6cb6331df6b14970f35d45f3348
Antonio <rssntn67@yahoo.it>
2018-06-08 09:17:23 +0200

    Cleaned JerseyclintImplementation

--- src/main/java/it/arsinfo/api/kettle/rest/client/JerseyClientImpl.java
+++ src/main/java/it/arsinfo/api/kettle/rest/client/JerseyClientImpl.java
@@ -8,7 +8,6 @@ import java.util.logging.Logger;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientHandlerException;
-import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.UniformInterfaceException;
 import com.sun.jersey.api.client.WebResource;
@@ -56,54 +55,7 @@ public class JerseyClientImpl {
     	}
     }
 
-    public <T> T get(Class<T> clazz,String relativePath) {
-    	try {
-    		return m_webResource.path(relativePath).accept(MediaType.APPLICATION_XML_TYPE).get(clazz);
-    	} catch (UniformInterfaceException uie) {
-    		logger.warning("GET: + "+ relativePath + "error: " + uie.getLocalizedMessage());
-    		throw uie;
-     	} catch (ClientHandlerException che) {
-    		logger.warning("GET: + "+ relativePath + "error: " + che.getLocalizedMessage());
-    		throw che;
-    	}
-    }
-    
-    public String getXml(String relativePath) {
-    	try {
-    		return m_webResource.path(relativePath).accept(MediaType.APPLICATION_XML_TYPE).get(String.class);
-    	} catch (UniformInterfaceException uie) {
-    		logger.warning("GET: + "+ relativePath + "error: " + uie.getLocalizedMessage());
-    		throw uie;
-     	} catch (ClientHandlerException che) {
-    		logger.warning("GET: + "+ relativePath + "error: " + che.getLocalizedMessage());
-    		throw che;
-    	}
-    }
-    
-    public String getXml(String relativePath,MultivaluedMap<String, String> queryParams) {
-    	try {
-    		return m_webResource.path(relativePath).queryParams(queryParams).accept(MediaType.APPLICATION_XML_TYPE).get(String.class);
-    	} catch (UniformInterfaceException uie) {
-    		logger.warning("GET: + "+ relativePath + "error: " + uie.getLocalizedMessage());
-    		throw uie;
-     	} catch (ClientHandlerException che) {
-    		logger.warning("GET: + "+ relativePath + "error: " + che.getLocalizedMessage());
-    		throw che;
-    	}
-    }
-
-    public String get(String relativePath) {
-    	try {
-    		return m_webResource.path(relativePath).accept(MediaType.TEXT_PLAIN).get(String.class);
-    	} catch (UniformInterfaceException uie) {
-    		logger.warning("GET: + "+ relativePath + "error: " + uie.getLocalizedMessage());
-    		throw uie;
-     	} catch (ClientHandlerException che) {
-    		logger.warning("GET: + "+ relativePath + "error: " + che.getLocalizedMessage());
-    		throw che;
-    	}
-    }
-    
+        
     public String get(String relativePath,MultivaluedMap<String, String> queryParams) {
     	try{
     		return m_webResource.path(relativePath).queryParams(queryParams).accept(MediaType.TEXT_PLAIN).get(String.class);
@@ -114,65 +66,6 @@ public class JerseyClientImpl {
     		logger.warning("GET: + "+ relativePath + " error: " + che.getLocalizedMessage());
     		throw che;
     	}
-    }
-
-    public void post(Object o, String relativePath) {
-    	try {
-    		ClientResponse cr = m_webResource.path(relativePath).type(MediaType.APPLICATION_XML_TYPE).post(ClientResponse.class,o);
-    		logger.info("POST: + "+ relativePath + " response: " + cr.getStatusInfo() + " Object: " + o );
-    	} catch (UniformInterfaceException uie) {
-    		logger.warning("POST: + "+ relativePath + " error: " + uie.getLocalizedMessage());
-    		throw uie;
-    	} catch (ClientHandlerException che) {
-    		logger.warning("POST: + "+ relativePath + " error: " + che.getLocalizedMessage());
-    		throw che;
-    	}
-    }
-    
-    public <T> T post(Class<T> clazz,String relativePath, MultivaluedMap<String, String> queryParams) {
-    	try {
-    		return m_webResource.path(relativePath).
-    				queryParams(queryParams).
-    				type(MediaType.APPLICATION_FORM_URLENCODED_TYPE).
-    				header("Accept", "application/xml").
-    				accept(MediaType.APPLICATION_XML_TYPE).post(new GenericType<T>(clazz));
-    	} catch (UniformInterfaceException uie) {
-    		logger.warning("POST: + "+ relativePath + "error: " + uie.getLocalizedMessage());
-    		uie.printStackTrace();
-    		throw uie;
-     	} catch (ClientHandlerException che) {
-    		logger.warning("POST: + "+ relativePath + "error: " + che.getLocalizedMessage());
-    		che.printStackTrace();
-    		throw che;
-    	}
-    }
-
-    
-    public void put(MultivaluedMap<String,String> mvm, String relativePath) {
-       	try {
-       		ClientResponse cr = m_webResource.queryParams(mvm).path(relativePath).type(MediaType.APPLICATION_FORM_URLENCODED_TYPE).put(ClientResponse.class);
-    		logger.info("PUT: + "+ relativePath + " response: " + cr.getStatusInfo());
-    	} catch (UniformInterfaceException uie) {
-    		logger.warning("PUT: + "+ relativePath + " error: " + uie.getLocalizedMessage());
-    		throw uie;
-    	} catch (ClientHandlerException che) {
-    		logger.warning("PUT: + "+ relativePath + " error: " + che.getLocalizedMessage());
-    		throw che;
-    	}
-    }
-    
-    public void delete(String relativePath) {
-    	try {
-       		ClientResponse cr = m_webResource.path(relativePath).delete(ClientResponse.class);
-    		logger.info("DELETE: + "+ relativePath + " response: " + cr.getStatusInfo());
-    	} catch (UniformInterfaceException uie) {
-    		logger.warning("DELETE: + "+ relativePath + " error: " + uie.getLocalizedMessage());
-    		throw uie;
-     	} catch (ClientHandlerException che) {
-    		logger.warning("DELETE: + "+ relativePath + " error: " + che.getLocalizedMessage());
-    		throw che;
-    	}
-
     }
 
 }
